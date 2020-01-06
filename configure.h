d38c0e0a333d403c31080b441fd8345f15f1f420
Fabio Kimura <fkimura@gmail.com>
2020-01-05 22:33:23 -0300

    Create configure.h

---
+++ configure.h
@@ -0,0 +1,13 @@
+// WIFI
+String ssid1 = "redewifi1";
+String ssid2 = "redewifi2";
+const char* password =  "senhadaredewifi";
+
+// OPENWEATHER API CONFIGURATION
+//String city =  "Sao Paulo,br";
+String key = "openweathermapapikey";
+String lugar = "lat=-23.585172&lon=-46.631224"; // sao paulo - vila mariana - ache sua localizacao no google maps
+// Time zone
+int timezone = -3;
+// 24H/12H time format (0 = 24H, 1 = 12H)
+byte time_format = 0;
